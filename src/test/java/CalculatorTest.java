a86a4502d076e3ecd715e3f1e3cc13044922b454
Luis Felipe <igorspopovich@hotmail.com>
2023-05-06 16:34:09 -0700

    Second commit

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,17 +3,17 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CalculatorTest {
+public class CalculatorTest {
 
     @Test
     @DisplayName("Add two numbers")
-    void add() {
+    public void add() {
         assertEquals(4, Calculator.add(2, 2));
     }
 
     @Test
     @DisplayName("Multiply two numbers")
-    void multiply() {
+    public void multiply() {
         assertAll(() -> assertEquals(4, Calculator.multiply(2, 2)),
                 () -> assertEquals(-4, Calculator.multiply(2, -2)),
                 () -> assertEquals(4, Calculator.multiply(-2, -2)),
@@ -22,7 +22,7 @@ class CalculatorTest {
 
     @Test
     @DisplayName("Divide two numbers")
-    void divide() {
+    public void divide() {
         assertAll(() -> assertEquals(1, Calculator.divide(2, 2)),
                 () -> assertEquals(-1, Calculator.divide(2, -2)),
                 () -> assertEquals(1, Calculator.divide(-2, -2)));
@@ -30,7 +30,7 @@ class CalculatorTest {
 
     @Test
     @DisplayName("Subtract two numbers")
-    void subtract() {
+    public void subtract() {
         assertAll(() -> assertEquals(1, Calculator.subtract(3, 2)),
                 () -> assertEquals(4, Calculator.subtract(2, -2)),
                 () -> assertEquals(-4, Calculator.subtract(-2, 2)));
